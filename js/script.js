97af97189ae5493d8396b35373599f25810df00e
Luca Isidoro <apprendista44@gmail.com>
2020-06-29 15:49:37 +0200

    esercizio base da sistemare

--- js/script.js
+++ js/script.js
@@ -4,47 +4,66 @@ $(document).ready(function() {
     "month" : 1,
     "year" : 2018,
   };
-  
+
   //Handlebars
   var source = $('#mese-template').html();
   var template = Handlebars.compile(source);
   var html = template();
   $('.container').append(html);
   $('.calendario h2').text(moment(data.day + "-" + data.year, "MM-YYYY").format("MMMM YYYY"))
-  // getDaysByMonth(month,year);
+
+  //creo Gennaio
+  var arrayMonth = getDaysByMonth(data.month,data.year);
+
+  currentMonthCalendary(data.month, arrayMonth);
+  // for (var i = 0; i < arrayMonth.length; i++) {
+  //   $(".days").append("<li>" + arrayMonth[i].format("D MMMM") + "</li>")
+  // }
 
 
 
-
-
-
-  console.log(getDaysByMonth(1,2020));
+  // console.log(getDaysByMonth(1,2020));
   // var array = getDaysByMonth(06,2020);
   // array[0].format("YYYY-MM-D");
   // console.log(  array[0].format("YYYY-MM-D"));
 
-  //prendo parametro mese da 1 a 12
-  function getDaysByMonth(month,year){
-    var daysInMonth = moment(year + "-" + month, "YYYY-MM").daysInMonth();
-    console.log(daysInMonth);
-    var arrayWithDays = [];
-    for (var i = 0; i<daysInMonth; i++) {
-      arrayWithDays[i] = moment([year,month-1,i+1])
-    }
-    return arrayWithDays;
-  }
 
 
   //trovo le festivita del mese con parametro mese da 1 a 12
-  function festivita(month,day) {
+  function currentMonthCalendary(month, arrayMonth) {
     month = month - 1;
     $.ajax(
       {
         url:' https://flynn.boolean.careers/exercises/api/holidays?year=2018&month=' + month,
         method: 'GET',
         success: function (data) {
-          console.log(data);
-          console.log(data.response);
+          //salvo le feste in un array bidimensionale
+          var feste = [
+            [],
+            []
+          ];
+          for (var i = 0; i < data.response.length; i++) {
+            feste[0][i] = data.response[i].date;
+          }
+          for (var i = 0; i < data.response.length; i++) {
+            feste[1][i] = data.response[i].name;
+          }
+          console.log(feste);
+
+          //scrivo il calendario
+          for (var i = 0; i < arrayMonth.length; i++) {
+            if (feste[0].includes(arrayMonth[i].format("YYYY-MM-DD"))) {
+              var indice = feste[0].indexOf(arrayMonth[i].format("YYYY-MM-DD"))
+              $(".days").append("<li class=\"festa\">" + arrayMonth[i].format("D MMMM") + "-" + "<span>" +feste[1][indice] + "</span>" + "</li>")
+
+            }else {
+              $(".days").append("<li>" + arrayMonth[i].format("D MMMM") + "</li>")
+
+            }
+            // $(".days").append("<li>" + arrayMonth[i].format("D MMMM") + "</li>")
+          }
+
+
         },
         error: function() {
             alert("Si è verificato un errore");
@@ -55,20 +74,18 @@ $(document).ready(function() {
   }
 
 
+  //prendo parametro mese da 1 a 12
+  //ritorno array con giorni del mese
+  function getDaysByMonth(month,year){
+    //catturo giorni in un mese
+    var daysInMonth = moment(year + "-" + month, "YYYY-MM").daysInMonth();
+    console.log(daysInMonth);
+    var arrayWithDays = [];
+    for (var i = 0; i<daysInMonth; i++) {
+      arrayWithDays[i] = moment([year,month-1,i+1])
+    }
+    return arrayWithDays;
+  }
 
-
-
-//   function getDaysArrayByMonth() {
-//   var daysInMonth = moment().daysInMonth();
-//   var arrDays = [];
-//
-//   while(daysInMonth) {
-//     var current = moment().date(daysInMonth);
-//     arrDays.push(current);
-//     daysInMonth--;
-//   }
-//
-//   return arrDays;
-// }
 
 });
